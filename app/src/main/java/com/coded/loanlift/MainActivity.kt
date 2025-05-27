04e2a5d9db71ab3e3e6ed0349b225026b7d992d9
mishalalquraini <alqurainimishal@gmail.com>
2025-05-27 20:38:34 +0300

    front page layout is done.

--- app/src/main/java/com/coded/loanlift/MainActivity.kt
+++ app/src/main/java/com/coded/loanlift/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.coded.loanlift.dashboardscreen.DashboardScreen
 import com.coded.loanlift.ui.theme.LoanLiftTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoanLiftTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DashboardScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DashboardScreenPreview() {
     LoanLiftTheme {
-        Greeting("Android")
+        DashboardScreen()
     }
 }
