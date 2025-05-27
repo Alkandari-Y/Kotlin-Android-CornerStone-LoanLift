ce6bce3dbacec77f8dd59e0c30fcc02f94d9cd63
Yousef AlKandari <yayalkandari@gmail.com>
2025-05-27 20:17:35 +0300

    dtos done??

--- app/src/main/java/com/coded/loanlift/data/response/UpdatePledgeRequest.kt
+++ app/src/main/java/com/coded/loanlift/data/response/UpdatePledgeRequest.kt
@@ -1,13 +1,8 @@
-package com.project.campaignlift.pledges.dtos
+package com.coded.loanlift.data.response
 
-import jakarta.validation.constraints.DecimalMin
-import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 
 data class UpdatePledgeRequest(
-
-    @field:NotNull(message = "Amount is required.")
-    @field:DecimalMin(value = "1000.000", message = "Amount must be at least 1000.000")
     val amount: BigDecimal?
 
 )
